665ce3b903e114118473a8237e2bf54455f8ff99
Andy Somogyi <andy.somogyi@gmail.com>
2017-06-27 13:22:26 -0400

    remove glm dep

--- src/MxMesh.h
+++ src/MxMesh.h
@@ -9,16 +9,12 @@
 #define _INCLUDE_MXMESH_H_
 
 #include "mechanica_private.h"
-#include "glm/glm.hpp"
+
 
 /**
  * Internal implementation of MxObject
  */
 struct MxMesh : MxObject {
-	glm::vec3 *positions;
-	glm::vec3 *normals;
-	glm::vec3 *velocties;
-	glm::ivec2 *texCoords;
 
 };
 
