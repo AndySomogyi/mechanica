bcd5f913c62b3de5f9165e6ce6005fec56fa04ba
Andy Somogyi <andy.somogyi@gmail.com>
2017-07-05 13:30:45 -0400

    add primitives example

---
+++ magnum_examples/primitives/PrimitivesExample.cpp
@@ -0,0 +1,146 @@
+/*
+    This file is part of Magnum.
+
+    Original authors — credit is appreciated but not required:
+
+        2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017 —
+            Vladimír Vondruš <mosra@centrum.cz>
+
+    This is free and unencumbered software released into the public domain.
+
+    Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+    this software, either in source code form or as a compiled binary, for any
+    purpose, commercial or non-commercial, and by any means.
+
+    In jurisdictions that recognize copyright laws, the author or authors of
+    this software dedicate any and all copyright interest in the software to
+    the public domain. We make this dedication for the benefit of the public
+    at large and to the detriment of our heirs and successors. We intend this
+    dedication to be an overt act of relinquishment in perpetuity of all
+    present and future rights to this software under copyright law.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <Magnum/Buffer.h>
+#include <Magnum/DefaultFramebuffer.h>
+#include <Magnum/Renderer.h>
+#include <Magnum/MeshTools/Interleave.h>
+#include <Magnum/MeshTools/CompressIndices.h>
+#include <Magnum/Platform/GlfwApplication.h>
+#include <Magnum/Primitives/Cube.h>
+#include <Magnum/Shaders/Phong.h>
+#include <Magnum/Trade/MeshData3D.h>
+
+using namespace Magnum;
+using namespace Magnum::Math::Literals;
+
+class PrimitivesExample: public Platform::GlfwApplication {
+    public:
+        explicit PrimitivesExample(const Arguments& arguments);
+
+    private:
+        void drawEvent() override;
+        void mousePressEvent(MouseEvent& event) override;
+        void mouseReleaseEvent(MouseEvent& event) override;
+        void mouseMoveEvent(MouseMoveEvent& event) override;
+
+        Buffer _indexBuffer, _vertexBuffer;
+        Mesh _mesh;
+        Shaders::Phong _shader;
+
+        Matrix4 _transformation, _projection;
+        Vector2i _previousMousePosition;
+        Color3 _color;
+};
+
+PrimitivesExample::PrimitivesExample(const Arguments& arguments):
+    Platform::GlfwApplication{arguments, Configuration{}
+        .setTitle("Magnum Primitives Example")} {
+    Renderer::enable(Renderer::Feature::DepthTest);
+    Renderer::enable(Renderer::Feature::FaceCulling);
+
+    const Trade::MeshData3D cube = Primitives::Cube::solid();
+
+    _vertexBuffer.setData(MeshTools::interleave(cube.positions(0), cube.normals(0)), BufferUsage::StaticDraw);
+
+    Containers::Array<char> indexData;
+    Mesh::IndexType indexType;
+    UnsignedInt indexStart, indexEnd;
+    std::tie(indexData, indexType, indexStart, indexEnd) = MeshTools::compressIndices(cube.indices());
+    _indexBuffer.setData(indexData, BufferUsage::StaticDraw);
+
+    _mesh.setPrimitive(cube.primitive())
+        .setCount(cube.indices().size())
+        .addVertexBuffer(_vertexBuffer, 0, Shaders::Phong::Position{}, Shaders::Phong::Normal{})
+        .setIndexBuffer(_indexBuffer, 0, indexType, indexStart, indexEnd);
+
+    _transformation = Matrix4::rotationX(30.0_degf)*
+                      Matrix4::rotationY(40.0_degf);
+    _color = Color3::fromHsv(35.0_degf, 1.0f, 1.0f);
+
+    _projection = Matrix4::perspectiveProjection(35.0_degf, Vector2{defaultFramebuffer.viewport().size()}.aspectRatio(), 0.01f, 100.0f)*
+                  Matrix4::translation(Vector3::zAxis(-10.0f));
+}
+
+void PrimitivesExample::drawEvent() {
+    defaultFramebuffer.clear(FramebufferClear::Color|FramebufferClear::Depth);
+
+    _shader.setLightPosition({7.0f, 5.0f, 2.5f})
+        .setLightColor(Color3{1.0f})
+        .setDiffuseColor(_color)
+        .setAmbientColor(Color3::fromHsv(_color.hue(), 1.0f, 0.3f))
+        .setTransformationMatrix(_transformation)
+        .setNormalMatrix(_transformation.rotationScaling())
+        .setProjectionMatrix(_projection);
+    _mesh.draw(_shader);
+
+    swapBuffers();
+}
+
+void PrimitivesExample::mousePressEvent(MouseEvent& event) {
+    if(event.button() != MouseEvent::Button::Left) return;
+
+    _previousMousePosition = event.position();
+    event.setAccepted();
+}
+
+void PrimitivesExample::mouseReleaseEvent(MouseEvent& event) {
+    _color = Color3::fromHsv(_color.hue() + 50.0_degf, 1.0f, 1.0f);
+
+    event.setAccepted();
+    redraw();
+}
+
+void PrimitivesExample::mouseMoveEvent(MouseMoveEvent& event) {
+
+    if(glfwGetMouseButton(window(), GLFW_MOUSE_BUTTON_1) != GLFW_PRESS) return;
+
+    const Vector2 delta = 3.0f*
+        Vector2{event.position() - _previousMousePosition}/
+        Vector2{defaultFramebuffer.viewport().size()};
+
+    _transformation =
+        Matrix4::rotationX(Rad{delta.y()})*
+        _transformation*
+        Matrix4::rotationY(Rad{delta.x()});
+
+    _previousMousePosition = event.position();
+    event.setAccepted();
+    redraw();
+}
+
+
+
+
+
+
+int main(int argc, char** argv) {
+    PrimitivesExample app({argc, argv});
+    return app.exec();
+}
