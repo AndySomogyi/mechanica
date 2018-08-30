a8b498891820a78586472dafe911b487012fb9ad
Andy Somogyi <andy.somogyi@gmail.com>
2018-08-30 10:26:58 -0400

    update

--- src/T3Transition.h
+++ src/T3Transition.h
@@ -14,6 +14,20 @@
 HRESULT applyT3PolygonSemiMajorAxisAngleTransition(MeshPtr mesh, PolygonPtr poly,
         float radians, PolygonPtr *p1, PolygonPtr *p2);
 
+/**
+ * Apply a T3 (split) operation to a polygon where the vertices are bisected
+ * by a plane. The plane is specified in point-normal form, where the point is
+ * the center of mass of this polygon, and the normal is given in normal.
+ *
+ * The plane normal should be roughly perpendicular to the face normal
+ * of the polygon. If the plane does not bisect any vertices, then an error
+ * is returned. The vertices of the polygon are partitioned into two sections:
+ * above and below the plane, and the polygon is split between between these
+ * two sections.
+ */
+HRESULT applyT3PolygonBisectPlaneTransition(MeshPtr mesh, PolygonPtr poly,
+        Vector3 *normal, PolygonPtr *p1, PolygonPtr *p2);
+
 
 
 #endif /* SRC_T3TRANSITION_H_ */
