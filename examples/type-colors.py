6144e26d23458160762c3f29703a665c55bf90ed
Andy Somogyi <andy.somogyi@gmail.com>
2020-07-31 17:54:40 -0400

    demo type colors

---
+++ examples/type-colors.py
@@ -0,0 +1,30 @@
+import mechanica as m
+import numpy as np
+
+# dimensions of universe
+dim=[20., 20., 20.]
+
+# new simulator, don't load any example
+m.Simulator(example="", dim=dim)
+
+# loop over arays for x/y coords
+for x in np.arange(0., 20., 2.):
+    for y in np.arange(0., 20., 2.):
+
+        # create a new particle type, chooses next default color
+        class P(m.Particle):
+            radius = 2
+
+        # instantiate that type
+        P([x+1.5, y+1.5, 10.])
+
+# run the simulator interactive
+m.Simulator.run()
+
+
+
+
+
+
+
+
