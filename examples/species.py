2855e13ba4384b9c3167e643a537518adcc7591e
Andy Somogyi <andy.somogyi@gmail.com>
2020-12-18 04:39:16 -0500

    updates

--- examples/species.py
+++ examples/species.py
@@ -2,6 +2,19 @@ import mechanica as m
 
 m.Simulator()
 
+print(m.carbon.Species("S1"))
+
+s1 = m.carbon.Species("$S1")
+
+s1 = m.carbon.Species("const S1")
+
+s1 = m.carbon.Species("const $S1")
+
+s1 = m.carbon.Species("S1 = 1")
+
+s1 = m.carbon.Species("const S1 = 234234.5")
+
+
 class A(m.Particle):
     species = ['S1', 'S2', 'S3']
 
