079719438b81b1eadbc5aafec951c56571cb0758
Andy Somogyi <andy.somogyi@gmail.com>
2021-01-04 01:37:15 -0500

    tweak

--- examples/fluxtest2.py
+++ examples/fluxtest2.py
@@ -3,17 +3,17 @@ import mechanica as m
 m.Simulator(dim=[6.5, 6.5, 6.5], bc=m.FREESLIP_FULL)
 
 class A (m.Particle):
+    radius = 0.1
     species = ['S1', 'S2', 'S3']
     style = {"colormap" : {"species" : "S1",
-                             "map" : "rainbow",
-                             "range" : "auto"}}
-    radius = 0.1
+                           "map" : "rainbow",
+                           "range" : "auto"}}
+
+m.flux(A, A, "S1", 5)
 
 uc = m.lattice.sc(0.25, A)
 
 parts = m.lattice.create_lattice(uc, [25, 25, 25])
-
-m.flux(A, A, "S1", 5)
 
 parts[24,24,24][0].species.S1 = 5000
 
