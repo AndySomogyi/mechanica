92df8a5a49dc4e31e02ab769a0379bde7e354c96
Andy Somogyi <andy.somogyi@gmail.com>
2021-02-01 21:10:22 -0500

    update example

--- examples/bonded_sc_lattice.py
+++ examples/bonded_sc_lattice.py
@@ -1,52 +1,46 @@
 import mechanica as m
 import numpy as np
 
-m.Simulator(dt=0.1, bc=m.FREESLIP_FULL)
+m.Simulator(dt=0.1, dim=[15, 12, 10]) #, bc=m.FREESLIP_FULL)
 
 # lattice spacing
-a = 1.0
+a = 0.65
 
 class A (m.Particle):
-    radius = 0.4
-    style={"color":"green"}
+    radius = 0.3
+    style={"color":"seagreen"}
     dynamics = m.Overdamped
 
 class B (m.Particle):
-    radius = 0.4
+    radius = 0.3
     style={"color":"red"}
     dynamics = m.Overdamped
 
 class Fixed (m.Particle):
-    radius = 0.4
+    radius = 0.3
     style={"color":"blue"}
     frozen = True
 
-
-repulse = m.Potential.coulomb(q=1, min=0.05, max=2)
+repulse = m.Potential.coulomb(q=0.08, min=0.01, max=2*a)
 
 m.bind(repulse, A, A)
 m.bind(repulse, A, B)
-m.bind(repulse, A, Fixed)
 
-def make_force():
-    return [0.48, 1 * np.sin( 0.4 * m.Universe.time), 0]
-
-f = m.forces.ConstantForce(make_force, 0.01)
+f = m.forces.ConstantForce(
+    lambda: [0.3, 1 * np.sin( 0.4 * m.Universe.time), 0], 0.01)
 
 m.bind(f, B)
 
-pot = m.Potential.power(r0=a/2, alpha=2)
+pot = m.Potential.power(r0=0.5*a, alpha=2)
 
-uc = m.lattice.sc(a, A, lambda i, j: m.Bond(pot, i, j, dissociation_energy=1.21))
+uc = m.lattice.sc(a, A,
+                  lambda i, j: m.Bond(pot, i, j, dissociation_energy=1.3))
 
-parts = m.lattice.create_lattice(uc, [10, 10, 10])
+parts = m.lattice.create_lattice(uc, [15, 15, 15])
 
-for p in parts[9,:].flatten():
-    p[0].become(B)
+for p in parts[14,:].flatten(): p[0].become(B)
 
-
-for p in parts[0,:].flatten():
-    p[0].become(Fixed)
+for p in parts[0,:].flatten(): p[0].become(Fixed)
 
 m.show()
 
