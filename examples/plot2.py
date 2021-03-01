28f40cf48562da1e1648ee5eab36378462c968a6
Andy Somogyi <andy.somogyi@gmail.com>
2021-03-01 00:03:34 -0500

    update

--- examples/plot2.py
+++ examples/plot2.py
@@ -1,6 +1,6 @@
 import mechanica as m
 
-m.Logger.set_level(m.Logger.LOG_DEBUG)
+m.Logger.set_level(m.Logger.LOG_TRACE)
 
 def plot_glj(r0, e=1, rr0=1, _m=2, n=3):
     p = m.Potential.glj(e=e, r0=rr0, m=_m, n=n, min=0.01, max=10)
@@ -10,9 +10,9 @@ def plot_glj(r0, e=1, rr0=1, _m=2, n=3):
 
 def plot_glj2(r0, e=1, rr0=1, _m=2, n=3):
 
-    p = m.Potential.glj(e=30, m=2, max=10)
+    p = m.Potential.glj(e=30, m=4, n=2, max=10)
 
-    p.plot(s=r0, min=0.0, max=1, potential=True, force=True, ymin=-1000000000, ymax=1000000)
+    p.plot(s=r0, min=0.7, max=1.7, potential=False, force=True)
 
 
 
@@ -23,4 +23,4 @@ radius = 0.01
 
 #p.plot()
 
-plot_glj2(4)
+plot_glj2(1)
