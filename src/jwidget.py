73e573aa8c810f0039d3538938059985b6135c6f
fanbsun <fanbsun@indiana.edu>
2021-04-05 16:02:19 -0400

    update jwigets.py

--- src/jwidget.py
+++ src/jwidget.py
@@ -4,6 +4,7 @@ import numpy as np
 import threading
 import time
 from ipyevents import Event 
+from IPython.display import display
 
 flag = False
 downflag = False
@@ -88,7 +89,7 @@ def init(*args, **kwargs):
 	reset.observe(onToggleReset,'value')
 
 	box = widgets.VBox([w, buttons])
-	#display(box)
+	display(box)
 
 	def background_threading():
 		global flag
